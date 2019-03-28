3f93af4a702d48301e120d851e6d5252fed839d9
Anna Dubov <a.chistyakova@gmail.com>
2019-03-28 21:56:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,6 @@ function addElementToBeginningOfArray (array, element) {
   return [element, ...array];
 }
 
-function 
+function addElementToEndOfArray (array, element) {
+  return [...array, element];
+}
